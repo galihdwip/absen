f22c35bd50d1da28ac0a6b9beb2e3c6c04a3a829
galihdwip <88543694+galihdwip@users.noreply.github.com>
2021-08-06 20:02:29 +0700

    Mengubah h1 pada form login
    asdsadasd

--- form_login.php
+++ form_login.php
@@ -38,7 +38,7 @@
           <div class="row justify-content-center">
             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
 			<img src="<?=base_url('')?>/assets/assets/img/brand/unla.png" width="100px">
-              <h1 class="text-white">Selamat Datang</h1>
+              <h1 class="text-white">Selamat Datang di Unla</h1>
               <p class="text-lead text-white">Login Absensi Universitas Langlangbuana</p>
             </div>
           </div>
